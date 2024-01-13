ea3f46aedb3853dfd9abe164073ba7d9d1344574
yongwoon <yongwoon.kim@webhani.com>
2024-01-13 15:18:48 +0900

    update layouts

--- src/components/organisms/AreaConsultant.tsx
+++ src/components/organisms/AreaConsultant.tsx
@@ -4,30 +4,17 @@ import Image from "next/image";
 export default function AreaConsultant() {
   return (
     <AreaTemplate isEven={false} sectionId={"area-consultant"}>
-      <div className="grid grid-cols-3 pt-10 px-10 gap-6 xl:gap-16 pb-12 sm:px-10 lg:px-20 xl:px-40 2xl:px-56 py-6 sm:py-8 lg:py-12 xl:py-16">
-        <div className="col-span-2">
-          <h2 className="text-4xl">コンサールタント</h2>
+      <div className="flex flex-auto pb-5">
+        <div className="item-center mt-3 sm:px-9 lg:px-18">
+          <h2 className="text-4xl text-center">コンサールタント</h2>
           <p className="text-xl pt-6">
             最適なウェブ開発戦略をご提案。ニーズに合わせたカスタムソリューションでビジネスの成功を支援。最新技術の導入やセキュリティ対策にも力を入れ、お客様のオンラインプレゼンスを最大化します。エキスパートのコンサルティングで、効果的かつ効率的なウェブソリューションを構築し、競争力を向上させましょう
           </p>
-          <div className="pt-20">
+          <div className="flex justify-center pt-5 sm:pt-20">
             <button className="px-18 py-5 rounded-2xl border-2 border-rose-500 bg-white text-rose-500 hover:border-rose-500 hover:text-white hover:bg-rose-500 text-xl font-semibold">
               無料で相談する
             </button>
           </div>
-        </div>
-        <div className="flex content-center">
-          <Image
-            src="/consultant/consultant.png"
-            alt="home-01"
-            className="dark:invert"
-            width={518}
-            height={450}
-            style={{
-              width: "100%",
-              height: "auto",
-            }}
-          />
         </div>
       </div>
     </AreaTemplate>
